08a54af635a2b99f4dd2dfc4ee05e9705d1036b1
Kevin Aoun <kevin.w.aoun@gmail.com>
2023-05-01 09:34:58 +0300

    Added departure date

--- src/home.js
+++ src/home.js
@@ -6,6 +6,8 @@ const signinBtn = document.getElementById('signin-btn');
 const signupBtn = document.getElementById('signup-btn');
 const rideLinkBtn = document.getElementById('join-link');
 const createRideBtn = document.getElementById('create-ride');
+const departureDateInput = document.getElementById('departure-date');
+const sameDayRide = document.getElementById('same-day-ride');
 
 function openSigninDialog() {
     event.preventDefault();
@@ -45,3 +47,13 @@ signinBtn.addEventListener('click', openSigninDialog);
 signupBtn.addEventListener('click', openSignupDialog);
 rideLinkBtn.addEventListener('click', openLinkDialog);
 createRideBtn.addEventListener('click', openCreateRideDialog);
+
+sameDayRide.addEventListener('change', () => {
+    if (sameDayRide.checked) {
+        departureDateInput.disabled = true;
+        departureDateInput.classList.add('text-gray-400', 'opacity-50', 'cursor-not-allowed');
+    } else {
+        departureDateInput.disabled = false;
+        departureDateInput.classList.remove('text-gray-400', 'opacity-50', 'cursor-not-allowed');
+    }
+});
